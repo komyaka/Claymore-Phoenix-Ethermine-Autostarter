1f99fdbc2f2dd0450baa172bfe17adfbfde8114d
Komyak <34284729+komyaka@users.noreply.github.com>
2018-11-04 00:53:02 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,80 @@
+# Clay-Phoenix-Ethermine-Autostarter
+Phoenix,Claymore Devfee no Hash Stop ,Autostart the Miner and Go
+Common problem with Phoenixminer and Claymore Miner is when DEVFEE comes by and the third or fourth time does not catch Shares gives these miners warning and stops the miner, to remedy this is this startbat watchdog and this ensures that the miner starts again or if necessary restart the whole miner rig, you place the contents of the start debate of the miner in this watchdog bat, and works great, you can download it at: https://github.com/digitalpara/Clay-Phoenix-Ethermine-Autostarter
+
+Download: https://mirrorace.com/m/1qlvn
+
+Phoenixminer,Claymoreminer,Ethereum miner WatchDog by DiGiTaLPaRa (SiMSeK-GitHub)
+
+MinerWatch is a simple script (Windows/Linux) WachDog for ETHminer.
+
+Working on Windows (7 / 8 / 10) [x86 / x64] and Linux (Any Dist / Any Ver / Any Arch).
+
+Table of Contents
+Features
+Download
+Install
+Usage
+Feedback
+Maintainers
+Contribute
+License
+Donations
+Features
+Simple script (Windows/Linux) WachDog for Phoenixminer,Claymoreminer,Ethereum miner.
+
+Phoenix miner,Claymore miner,Ethereum miner WatchDog_SMSK:
+
+Run Phoenixminer,Claymore miner,Ethereum miner
+Restart Phoenixminer,Claymoreminer,Ethereum miner up to 5 times
+Reboot the system
+CUDA support removed because Phoenixminer,Claymoreminer,Ethereum miner added --exit parameter to exit whenever an error occurred! ([#757] P.R.)
+AutoFix #189 issue of Phoenixminer,Claymoreminer,Ethereum miner.
+AutoFix #385 issue of Phoenixminer,Claymoreminer,Ethereum miner.
+Log file RunTimes.log.
+Auto Turn off the Error Dialog (Windows).
+On all systems, it is necessary for the user to have reboot permissions.
+Οn Linux systems create a file /etc/sudoers.d/reboot with username ALL=NOPASSWD:/sbin/reboot
+Download
+Download Last release here.
+
+Install
+Script (No installation required)
+
+Usage
+Download or Clone
+Unzip
+Copy minerWatchDog.bat (Windows) or minerWatchDog.sh (Linux) to Phoenixminer,Claymoreminer,Ethereum miner folder.
+Modify the code between:
+
+==================== Your Code Starts Here ====================
+
+==================== Your Code Ends Here ====================
+
+
+Run the script.
+
+Feedback
+Please inform me for additional improvements; open an issue.
+
+If you like please give a GitHub Star (it's free!).
+
+Maintainers
+Simsek (digitalpara-GitHub).
+
+Contribute
+Feel free to open an Issue or submit Pull Requests.
+
+License
+MIT © simsek (Digitalpara-GitHub).
+
+Donations
+If you find this useful please donate something at the following:
+
+Bitcoin Address (BTC): 1P4WiVsZ8H7PHKNBXyXzZwqxnEjot7UXfM
+
+Ethereum Address (ETH): 0xcf3cdd09dcfe2d5f9dd5e91bb25d083d5f3a6671
+
+Dogecoin Address (DGC): DQNSpJbg7trxX2Ecof6MqLTvhhaszr7HC5
+
+Redcoin Address (RC): RmrmNoNkzDpjbpDQNRuuHKCSSaxoUMcb5R
---
+++ start_Ethminer_watchdogstarter.bat
@@ -0,0 +1,180 @@
+rem Skip RunEthMinerCommand section at start
+goto PreStart
+
+rem ========== Run EthMiner Command ==========
+
+:RunEthMinerCommand
+
+rem ==================== Your Code Starts Here ====================
+rem ==================== Your Code Starts Here ====================
+rem ==================== Your Code Starts Here ====================
+
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+
+rem set NO_COLOR=Yes
+
+ethminer.exe -RH -X --exit -U -P stratum+ssl://0xcf3cdd09dcfe2d5f9dd5e91bb25d083d5f3a6671.rig4@eu1.ethermine.org:5555 -P stratum+ssl://0xcf3cdd09dcfe2d5f9dd5e91bb25d083d5f3a6671.rig4@us1.ethermine.org:5555
+
+rem ==================== Your Code Ends Here ====================
+rem ==================== Your Code Ends Here ====================
+rem ==================== Your Code Ends Here ====================
+
+exit /b
+
+rem ========== PreStart ==========
+
+:PreStart
+
+rem Don't echo to standard output
+@echo off
+rem Make script variables local
+setlocal
+rem Set version info
+set V=1.9.4 (Donate ETH:0xcf3cdd09dcfe2d5f9dd5e91bb25d083d5f3a6671)
+rem Switch to the batch file's directory
+cd /d %~dp0
+rem Set codepage
+chcp 437 >nul
+rem Set title
+title PHOENiX WatchDog Version %V% by DiGiTaLPaRa_SiMSEK_2018
+
+rem ========== Start ==========
+
+cls
+echo ###############################################################################
+echo.
+echo   ETHERMiNERWatchDog Version %V%
+echo.
+echo   AUTHOR: SiMSeK  (DiGiTaLPaRa - TO-GitHub)
+echo.
+echo ###############################################################################
+echo.
+echo ETHERMiNERWatchDog
+echo 1. Run ethminer.
+echo 2. Restart ethminer up to 5 times.
+echo 3. Reboot the system.
+echo.
+echo Additional:
+echo - AutoFix #385 issue of Ethminer
+echo - AutoFix #189 issue of Ethminer
+echo - Log file RunTimes.log
+echo - Auto Turn off the Error Dialog
+echo.
+
+rem ========== Initializing ==========
+
+:Initializing
+rem set loop to zero
+set /A loopnum=0
+set FileOut=RunTimes.log
+echo ETHminerWatchDogDmW Version %V% >> %FileOut%
+reg add "HKCU\SOFTWARE\Microsoft\Windows\Windows Error Reporting" /v "DontShowUI" /t REG_DWORD /d 1 /f > nul 2>&1
+
+rem ========== Run Program ==========
+
+:runProgram
+
+rem ========== Calc ==========
+
+rem Increment loop by one
+set /A loopnum=loopnum+1
+
+rem Calculate Date & Time
+:DateTime
+rem Check if WMIC is available
+WMIC.EXE Alias /? >nul 2>&1 || goto wmicError
+rem Use WMIC to retrieve date and time
+for /F "skip=1 tokens=1-6" %%G in ('WMIC Path Win32_LocalTime Get Day^,Hour^,Minute^,Month^,Second^,Year /Format:table') do (
+   if "%%~L"=="" goto wmicDone
+      set _yyyy=%%L
+      set _mm=00%%J
+      set _dd=00%%G
+      set _hour=00%%H
+      set _minute=00%%I
+)
+:wmicDone
+
+rem Pad digits with leading zeros
+      set _mm=%_mm:~-2%
+      set _dd=%_dd:~-2%
+      set _hour=%_hour:~-2%
+      set _minute=%_minute:~-2%
+
+rem Date/time in ISO 8601 format:
+set pISOdate=%_yyyy%-%_mm%-%_dd% %_hour%:%_minute%
+
+goto DateTimeOK
+
+:wmicError
+for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set pdate=%%c-%%a-%%b)
+for /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set ptime=%%a:%%b)
+set pISOdate=%pdate% %ptime%
+
+:DateTimeOK
+
+rem ========== Output ==========
+
+rem ========== Screen Output ==========
+
+echo.
+echo ===============================================================================
+echo %pISOdate%
+echo ETHERMiNERWatchDog_SMSK has run %loopnum% times.
+echo ===============================================================================
+echo.
+
+rem ========== File Output ==========
+
+echo %pISOdate% ETHERMiNERWatchDog_SMSK has run %loopnum% times.>>%FileOut%
+
+rem ========== Execution Code ==========
+
+call :RunEthMinerCommand
+
+rem Wait 5s
+timeout /T 5 /NOBREAK>NUL
+
+rem Check 5 loops
+if %loopnum% gtr 4 goto ErrorHandling
+
+rem Loop
+goto runProgram
+
+rem ========== Error Handling ==========
+
+:ErrorHandling
+
+rem ========== Error Screen Output ==========
+
+echo.
+echo ===============================================================================
+echo %pISOdate%
+echo ETHERMiNERWatchDog_SMSK has run %loopnum% times.
+echo System restart required.
+echo.
+echo.
+echo.
+echo Rebooting now (%pISOdate%).
+echo ###############################################################################
+echo.
+
+rem ========== Error File Output ==========
+
+echo %pISOdate% ETHERMiNERWatchDog_SMSK has run %loopnum% times.>>%FileOut%
+echo System restart required. Rebooting now (%pISOdate%).>>%FileOut%
+echo. >>%FileOut%
+echo. >>%FileOut%
+
+rem ========== System Reboot ==========
+
+shutdown -r -f -t 0
+
+rem ========== End ==========
+
+endlocal
+
+rem ========== EoF ==========
